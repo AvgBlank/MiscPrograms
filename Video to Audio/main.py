f7b12d0f37aee83585c9ff504c5018bb353cca5e
Husain Khorakiwala <112507212+AverageBlank@users.noreply.github.com>
2024-06-20 18:36:20 +0530

    Small Changes

--- Video to Audio/main.py
+++ Video to Audio/main.py
@@ -2,15 +2,18 @@ from moviepy.editor import VideoFileClip
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-# Load the mp4 file
-file = filedialog.askopenfilename(initialdir = "~",title = "Select file",filetypes = ((
-        ('Video', '*.mkv'), 
-        ('Video', '*.mp4'), 
-        ('All Files', '*.*'))))
-print(file)
-video = VideoFileClip(file)
+def vidToAud():
+        file = filedialog.askopenfilename(initialdir = "~",title = "Select file",filetypes = ((
+                ('Video', '*.mkv'), 
+                ('Video', '*.mp4'), 
+                ('All Files', '*.*'))))
+        print(file)
+        video = VideoFileClip(file)
+        
+        # Extract audio from video
+        save = asksaveasfilename(filetypes=[("Audio - MP3", "*.mp3")], defaultextension=[("Audio - MP3", "*mp3")])
+        print(save)
+        video.audio.write_audiofile(save)
 
-# Extract audio from video
-save = asksaveasfilename(filetypes=[("Audio - MP3", "*.mp3")], defaultextension=[("Audio - MP3", "*mp3")])
-print(save)
-video.audio.write_audiofile(save)
+if __name__ == "__main__":
+        vidToAud()
